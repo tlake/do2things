da11578d530c09cb6f06a9888da7f60d9969b33c
tlake <tanner.lake@gmail.com>
2015-07-24 11:27:50 -0700

    quantum selection works! core functionality is now complete

--- do2things/decohere/views.py
+++ do2things/decohere/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView, FormView
+from django.shortcuts import render_to_response
 from .forms import TwoThingsForm
+from quantumrandom import randint
 
 
 class HomePageView(TemplateView):
@@ -10,3 +12,15 @@ class DecohereView(FormView):
     template_name = 'decohere.html'
     form_class = TwoThingsForm
     success_url = '/decohere'
+
+    def form_valid(self, form):
+        thing_to_do = form.data['thing_1']
+
+        if randint(0, 2) > 0:
+            thing_to_do = form.data['thing_2']
+
+        return render_to_response('dothis.html', {'thing_to_do': thing_to_do})
+
+
+class DoThisView(TemplateView):
+    template_name = 'dothis.html'
