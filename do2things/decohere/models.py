7b4dab1d8eaff871b786700e4cccd5a77a092722
tlake <tanner.lake@gmail.com>
2015-07-27 23:03:55 -0700

    updated for python2 backwards-compatibility

--- do2things/decohere/models.py
+++ do2things/decohere/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
+from django.db import models
 
 
 @python_2_unicode_compatible
@@ -8,8 +10,6 @@ class Decoherence(models.Model):
     state2 = models.CharField(max_length=127)
     date_created = models.DateTimeField(auto_now_add=True)
     decohered = models.CharField(max_length=127, blank=True)
-
-    objects = models.Manager
 
     def __str__(self):
         return 'Decoherence at {}'.format(self.date_created)
