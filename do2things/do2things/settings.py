618b60199e2542183edbf37a8c95d8e69d318ecd
tlake <tanner.lake@gmail.com>
2015-08-26 19:40:37 -0700

    allowed hosts update

--- do2things/do2things/settings.py
+++ do2things/do2things/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'do2things.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'do2things.herokuapp.com', '*']
 
 
 # Application definition
